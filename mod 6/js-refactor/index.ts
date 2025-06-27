c69a07600091e4f9803d5026f6b0d988afc0d024
Artis <artisxwatson2005@gmail.com>
2025-06-27 10:46:50 -0400

    Delete mod 6 directory

--- mod 6/js-refactor/index.ts
+++
@@ -1,6 +0,0 @@
-// declare a variable
-let username: string = "Artis";
-
-let age: number = 11;
-
-
--- mod 6/sba mod/src/utils/retryPromise.ts
+++
@@ -1,6 +0,0 @@
-export function retryPromise<T>(fn: () => Promise<T>, retries: number, delay: number): Promise<T> {
-  return fn().catch((error) => {
-    if (retries <= 0) throw error;
-    return new Promise((resolve) => setTimeout(resolve, delay)).then(() => retryPromise(fn, retries - 1, delay));
-  });
-}
